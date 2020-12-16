d18d86beb8d70c8df4e6518217b27b1db15bace5
AzureAD\KarthickMani <karthick.mani@syncfusion.com>
2020-12-16 15:31:33 +0530

    WPF-55869 - Updated localization sample

---
+++ Model/Meeting.cs
@@ -0,0 +1,102 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Localization
+{
+    public class Meeting : NotificationObject
+    {
+        DateTime from, to;
+        string eventName;
+        bool isAllDay;
+        string startTimeZone, endTimeZone;
+        Brush backgroundColor;
+        Brush foregroundColor;
+
+        public Meeting()
+        {
+        }
+
+        public DateTime From
+        {
+            get { return from; }
+            set
+            {
+                from = value;
+                RaisePropertyChanged("From");
+            }
+        }
+
+        public DateTime To
+        {
+            get { return to; }
+            set
+            {
+                to = value;
+                RaisePropertyChanged("To");
+            }
+        }
+
+        public bool IsAllDay
+        {
+            get { return isAllDay; }
+            set
+            {
+                isAllDay = value;
+                RaisePropertyChanged("IsAllDay");
+            }
+        }
+        public string EventName
+        {
+            get { return eventName; }
+            set
+            {
+                eventName = value;
+                RaisePropertyChanged("EventName");
+            }
+        }
+        public string StartTimeZone
+        {
+            get { return startTimeZone; }
+            set
+            {
+                startTimeZone = value;
+                RaisePropertyChanged("StartTimeZone");
+            }
+        }
+        public string EndTimeZone
+        {
+            get { return endTimeZone; }
+            set
+            {
+                endTimeZone = value;
+                RaisePropertyChanged("EndTimeZone");
+            }
+        }
+
+        public Brush BackgroundColor
+        {
+            get { return backgroundColor; }
+            set
+            {
+                backgroundColor = value;
+                RaisePropertyChanged("BackgroundColor");
+            }
+        }
+
+        public Brush ForegroundColor
+        {
+            get { return foregroundColor; }
+            set
+            {
+                foregroundColor = value;
+                RaisePropertyChanged("ForegroundColor");
+            }
+        }
+    }
+}
